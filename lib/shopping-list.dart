54f71f5aabd90fc34f3a7b8fcee148be57a86e0f
Ersel26 <yigit.ersel@metu.edu.tr>
2022-03-11 16:27:59 +0300

    shopping list ui duzenlendi

--- lib/shopping-list.dart
+++ lib/shopping-list.dart
@@ -17,10 +17,7 @@ import 'package:flutter/material.dart';
   Ingredient(name: "olive oil", amountNum: 1, amountType: "L"),
 ];*/
 List<Ingredient> _all = all.values.toList();
-List<Ingredient> _needs = [
-  Ingredient(name: "rice", amountNum: 2, amountType: "kg"),
-  Ingredient(name: "olive oil", amountNum: 1, amountType: "L"),
-];
+List<Ingredient> _needs = [];
 List<Ingredient> _owned = [];
 
 class ShoppingList extends StatefulWidget {
@@ -32,43 +29,100 @@ class ShoppingList extends StatefulWidget {
 
 class _ShoppingListState extends State<ShoppingList> {
 
-  final background = Colors.grey.shade300;
-  final profileColor = Colors.red.shade700;
+  final titleColor = Color(0xff4297a0);
+  final titleTextColor = Color(0xffffffff);
+  final listBackground = Color(0xff9bc0c3);
+  final backgroundColor = Color(0xfff4eae6);
+  final itemTextColor = Color(0xffffffff);
 
 
   @override
   Widget build(BuildContext context) {
 
-    return Scaffold(
-      body: ListView.builder(
-          itemCount: _all.length,
-          itemBuilder: (context, i) {
-            if (i.isOdd) return const Divider();
-
-            final index = i ~/ 2;
-
-            return ListTile(
-              leading: Text(_all[index].amountNum.toString() + " " + _all[index].amountType),
-              title: Text(_all[index].name),
-              trailing: Icon(
-                !_needs.contains(_all[index]) ? Icons.check_box_outline_blank : Icons.check_box,
+    return Container(
+      color: backgroundColor,
+      child: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                color: titleColor,
+                height: 38,
+                width: 232,
+                child: Center(
+                  child: Text(
+                      "Shopping List",
+                      style: TextStyle(
+                        color: titleTextColor,
+                        fontSize: 18.0,
+                        fontFamily: 'Lexend Peta',
+                        fontWeight: FontWeight.w400,
+                      ),
+                  ),
+                ),
               ),
-              onTap: () {
-                setState(() {
-                  if(_needs.contains(_all[index])) {
-                    _needs.remove(_all[index]);
-                    _owned.add(_all[index]);
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: _all.length,
+                  itemBuilder: (context, i) {
+                    if (i.isOdd) return const Divider();
+
+                    final index = i ~/ 2;
+
+                    return Card(
+                      elevation: 0.0,
+                      color: listBackground,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: ListTile(
+                        leading: Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 4, 0, 0),
+                          child: Text(
+                              _all[index].amountNum.toString() + " " + _all[index].amountType,
+                              style: TextStyle(
+                                color: itemTextColor,
+                                fontFamily: 'Lexend Peta',
+                                fontWeight: FontWeight.w400,
+                              ),
+                          ),
+                        ),
+                        title: Text(
+                            _all[index].name,
+                            style: TextStyle(
+                              color: itemTextColor,
+                              fontFamily: 'Lexend Peta',
+                              fontWeight: FontWeight.w400,
+                            ),
+                        ),
+                        trailing: Icon(
+                          !_needs.contains(_all[index]) ? Icons.check_box_outline_blank : Icons.check_box,
+                        ),
+                        onTap: () {
+                          setState(() {
+                            if(_needs.contains(_all[index])) {
+                              _needs.remove(_all[index]);
+                              _owned.add(_all[index]);
+                            }
+                            else {
+                              _owned.remove(_all[index]);
+                              _needs.add(_all[index]);
+                            }
+                          });
+                        },
+                      ),
+                    );
                   }
-                  else {
-                    _owned.remove(_all[index]);
-                    _needs.add(_all[index]);
-                  }
-                });
-              },
-            );
-          }
+              ),
+            ),
+          ],
+        ),
       ),
     );
+
   }
 }
 
